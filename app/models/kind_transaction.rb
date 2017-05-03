18ad937902e946498fec4af815aeea1c167eba2d
samukamuniz <samuka.muniz@gmail.com>
2017-05-03 20:47:40 -0300

    Ajuste de identação

--- app/models/kind_transaction.rb
+++ app/models/kind_transaction.rb
@@ -1,6 +1,6 @@
-	class KindTransaction < ActiveRecord::Base
-		has_many :categories
-		has_many :transactions
-		validates_presence_of :description
-  	validates_uniqueness_of :description
+class KindTransaction < ActiveRecord::Base
+	has_many :categories
+	has_many :transactions
+	validates_presence_of :description
+	validates_uniqueness_of :description
 end
