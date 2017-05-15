45bbe56b7f9a3662f6dc641cb7d1d6fdd0e4808c
samukamuniz <samuka.muniz@gmail.com>
2017-05-15 18:08:26 -0300

    Mostrando usuário em contas

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,13 @@
 class Category < ActiveRecord::Base
   belongs_to :member
   has_many :transactions
-  enum kind_transaction: [:expenses, :incomes]
+  enum kind_transaction: {:expenses => 1, :incomes => 2}  
 
   def kind_transaction_br
   	if self.kind_transaction == 'expenses'
-  		'Despesasss'
+  		'Despesas'
   	else
-  		'Receitasss'
+  		'Receitas'
   	end  	
   end
 end
