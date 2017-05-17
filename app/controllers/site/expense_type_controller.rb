52be4b4c76d7817db4183bc72bda96a889b506f8
samukamuniz <samuka.muniz@gmail.com>
2017-05-17 19:24:31 -0300

    Configurando Layout Tipos de despesas

--- app/controllers/site/expense_type_controller.rb
+++ app/controllers/site/expense_type_controller.rb
@@ -1,4 +1,62 @@
-class Site::ExpenseTypeController < ApplicationController
+class Site::ExpenseTypeController < SiteController
+  before_action :set_expense_type, only: [:edit, :update, :destroy]
+
   def index
+    @expense_types = Category.where(kind_transaction: 1, member: current_member)
   end
+
+  def new
+    @expense_type = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @expense_type = Category.new(expense_type_params)
+    @expense_type.member = current_member
+    @expense_type.kind_transaction = 1
+
+    respond_to do |format|
+      if @expense_type.save
+        format.html { redirect_to site_expense_type_index_path, notice: "A Categoria (#{@expense_type.description}) foi salva com sucesso!" }
+        format.json { render :show, status: :created, location: @expense_type }
+      else
+        format.html { render :new }
+        format.json { render json: @expense_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @expense_type.update(expense_type_params)
+        format.html { redirect_to site_expense_type_index_path, notice: "A Categoria (#{@expense_type.description}) foi atualizada com sucesso!" }
+        format.json { render :show, status: :ok, location: @expense_type }
+      else
+        format.html { render :edit }
+        format.json { render json: @expense_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    tipoDespesa = @expense_type.description
+    @expense_type.destroy
+    respond_to do |format|
+      format.html { redirect_to site_expense_type_index_path, notice: "A Categoria (#{tipoDespesa}) foi deletada com sucesso!" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_expense_type
+      @expense_type = Category.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def expense_type_params
+      params.require(:expense_type).permit(:kind_transaction, :member_id, :description)
+    end
 end
