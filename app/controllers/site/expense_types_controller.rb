b6a7dbbdd2afcab14d81dc66378492a374207313
samukamuniz <samuka.muniz@gmail.com>
2017-05-20 18:31:48 -0300

    Ajustando expense_types

--- app/controllers/site/expense_types_controller.rb
+++ app/controllers/site/expense_types_controller.rb
@@ -1,48 +1,59 @@
-class Site::ExpenseTypesController < SiteController
-  before_action :set_expense_type, only: [:edit, :update, :destroy]
+class Site::ExpenseTypesController < Site::CategoriesController
+  before_action :set_category, only: [ :update, :destroy]
 
   def index
-    @expense_types = Category.where(kind_transaction: 1, member: current_member)
+    @categories = Category.where(kind_transaction: 1, member: current_member)
   end
 
   def new
-    @expense_type = Category.new
+    @category = Category.new
   end
 
   def edit
   end
 
   def create
-    @expense_type = Category.new(expense_type_params)
-    @expense_type.member = current_member
-    @expense_type.kind_transaction = 1
-
+    @category = Category.new(category_params)
+    @category.member = current_member
+    @category.kind_transaction = 1
+    puts "****************************************************************************************************"
+    puts "****************************************************************************************************"
+    puts "Estou passando por aqui expense_types_create"
+    puts "****************************************************************************************************"
+    puts "****************************************************************************************************"
     respond_to do |format|
-      if @expense_type.save
-        format.html { redirect_to site_expense_types_path, notice: "A Categoria (#{@expense_type.description}) foi salva com sucesso!" }
-        format.json { render :show, status: :created, location: @expense_type }
+      if @category.save
+        format.html { redirect_to site_expense_types_path, notice: "A Categoria (#{@category.description}) foi salva com sucesso!" }
+        format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
-        format.json { render json: @expense_type.errors, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
+    puts "****************************************************************************************************"
+    puts "****************************************************************************************************"
+    puts "Estou passando por aqui categories_update"
+    puts "****************************************************************************************************"
+    puts "****************************************************************************************************"
+    
     respond_to do |format|
-      if @expense_type.update(expense_type_params)
-        format.html { redirect_to site_expense_types_path, notice: "A Categoria (#{@expense_type.description}) foi atualizada com sucesso!" }
-        format.json { render :show, status: :ok, location: @expense_type }
+      if @category.update(category_params)
+        format.html { redirect_to site_expense_types_path, notice: "A Categoria (#{@category.description}) foi atualizada com sucesso!" }
+        format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
-        format.json { render json: @expense_type.errors, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
+
   def destroy
-    tipoDespesa = @expense_type.description
-    @expense_type.destroy
+    tipoDespesa = @category.description
+    @category.destroy
     respond_to do |format|
       format.html { redirect_to site_expense_types_path, notice: "A Categoria (#{tipoDespesa}) foi deletada com sucesso!" }
       format.json { head :no_content }
@@ -50,13 +61,14 @@ class Site::ExpenseTypesController < SiteController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_expense_type
-      @expense_type = Category.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def expense_type_params
-      params.require(:expense_type).permit(:kind_transaction, :member_id, :description)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def category_params
+    params.require(:category).permit(:kind_transaction, :member_id, :description)
+  end
+
 end
