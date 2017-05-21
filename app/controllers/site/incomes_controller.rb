cf7db084c23a10c69199c164f5536a7c59460763
samukamuniz <samuka.muniz@gmail.com>
2017-05-21 18:46:01 -0300

    Ajustando seleção de categorias no form em incomes

--- app/controllers/site/incomes_controller.rb
+++ app/controllers/site/incomes_controller.rb
@@ -9,6 +9,7 @@ class Site::IncomesController < Site::TransactionsController
   def new
     @transaction = Transaction.new
 		@anddress = site_incomes_path
+		@transaction_type = 2
   end
 
   def edit
@@ -32,6 +33,22 @@ class Site::IncomesController < Site::TransactionsController
         format.html { render :new }
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    transacao = @transaction.description
+    @transaction_save = Transaction.find(@transaction.id) #Busca no BD a Transação Existente e guarda os valores
+    @account_save = Account.find(@transaction_save.account_id) #Busca a conta inicial da transação existente
+    
+    @transaction.destroy
+    respond_to do |format|
+      format.html { redirect_to site_expenses_path, notice: "A Transação (#{transacao}) foi deletada com sucesso!" }
+      format.json { head :no_content }
+    end
+
+    if @transaction.paid == true
+      debit
     end
   end
 
--- app/helpers/site/incomes_helper.rb
+++ app/helpers/site/incomes_helper.rb
@@ -1,2 +1,11 @@
 module Site::IncomesHelper
+
+  def category_options
+    @kind = @transaction_type
+    @Kind_category = Category.where(kind_transaction: @kind, member_id: current_member)
+  end
+
+	def account_options
+    @accountUser = Account.where(member_id: current_member)
+  end
 end
