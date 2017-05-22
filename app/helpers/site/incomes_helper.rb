813dc7b80c990e1c29b67a7edfddbd078af92bb9
samukamuniz <samuka.muniz@gmail.com>
2017-05-21 23:26:40 -0300

    Configurando helper

--- app/helpers/site/incomes_helper.rb
+++ app/helpers/site/incomes_helper.rb
@@ -8,4 +8,8 @@ module Site::IncomesHelper
 	def account_options
     @accountUser = Account.where(member_id: current_member)
   end
+
+  def sum_income
+  	@sum_icomes = Transaction.where(member_id: current_member, kind_transaction: 2).sum("amount_cents")
+  end
 end
