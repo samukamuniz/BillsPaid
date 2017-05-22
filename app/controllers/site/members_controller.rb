c436a4ebca7e994fc276cfe5d419127df05abc6a
samukamuniz <samuka.muniz@gmail.com>
2017-05-22 00:44:39 -0300

    Configurando tela de edição de usuário

--- app/controllers/site/members_controller.rb
+++ app/controllers/site/members_controller.rb
@@ -1,22 +1,9 @@
 class Site::MembersController < SiteController
-	before_action :set_member, only: [:new,:create,:edit, :update]
+	before_action :set_member, only: [:edit, :update]
   
-  def new
-    @member = Member.new
-  end
-
   def edit
   end
   
-  def create
-    @member = Admin.new(members_params)
-    if @member.save
-      redirect_to site_home_path, notice: "<b>#{@member.name}</b>, seu usuário foi criado com sucesso!"  
-    else
-      render :new
-    end
-  end
-
   def update
     pwd = params[:member][:password]
     pwd_confirm = params[:member][:password_confirmation]
@@ -26,7 +13,7 @@ class Site::MembersController < SiteController
       params[:member].delete(:password_confirmation)
     end
 
-    if @member.update(member_params)
+    if @member.update(members_params)
       redirect_to site_home_path, notice: "<b>#{@member.name}</b>, seus dados foram atualizados com sucesso!"
     else
 	    render :edit
