6efe9dd5309b6810c2651919121d3c2f43f2cf2e
samukamuniz <samuka.muniz@gmail.com>
2017-05-21 21:15:06 -0300

    Organizando Tudo

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,9 +1,4 @@
 class Site::HomeController < SiteController 
   def index
-  	#@receitas = Account.sum("amount_cents", member: current_member)
-  	#@contas = Account.where(member: current_member).sum("amount_cents")
-  	#@receitas = Transaction.where(member: current_member, kind_transaction: 2)
-  	#@despesas = Transaction.where(member: current_member, kind_transaction: 1).sum("amount_cents")
- 
   end
 end
--- app/helpers/site/home_helper.rb
+++ app/helpers/site/home_helper.rb
@@ -1,2 +1,26 @@
 module Site::HomeHelper
+  def sald_to_account
+    @sald = Account.sum('amount_cents')
+  end
+
+  def total_incomes
+    @sald = Transaction.where(member: current_member, kind_transaction: 2).sum("amount_cents")
+  end
+
+  def total_expenses
+    @sald = Transaction.where(member: current_member, kind_transaction: 1).sum("amount_cents")
+  end
+
+
+
+
+
+
+
+
+
+
+  	#@contas = Account.where(member: current_member).sum("amount_cents")
+  	#@receitas = Transaction.where(member: current_member, kind_transaction: 2)
+  	#@despesas = Transaction.where(member: current_member, kind_transaction: 1).sum("amount_cents")
 end
